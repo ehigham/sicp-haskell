812ff5bfe46e8f62e258320e92602b575b414643
Edmund Higham <edhigham@gmail.com>
2020-01-04 23:58:08 +0000

    Exercise 1.1 completed using ghci

--- src/Chapter1/Exercise1.hs
+++ src/Chapter1/Exercise1.hs
@@ -1,4 +1,27 @@
 module Chapter1.Exercise1 where
--- Below is a sequence of expressions. What is the result printed by the
--- interpreter in response to each expression? Assume that the sequence is to
--- be evaluated in the order in which it is presented.
+-- | Below is a sequence of expressions. What is the result printed by the
+-- | interpreter in response to each expression? Assume that the sequence is to
+-- | be evaluated in the order in which it is presented.
+--
+-- >>> 10
+-- 10
+-- >>> plus [5, 3, 4]
+-- 12
+-- >>> (-) 9 1
+-- 8
+-- >>> (+) ((*) 2 4) ((-) 4 6)
+-- 6
+-- >>> let a = 3 :: Int
+-- >>> let b = (+) a 1
+-- >>> sum [a, b, (*) a b]
+-- 19
+-- >>> (==) a b
+-- False
+-- >>> if and [((>) b a), ((<) b ((*) a b))] then b else a
+-- 4
+-- >>> if a == 4 then 6 else if b == 4 then sum [6, 7, a] else 25
+-- 16
+-- >>> (+) 2 (if (>) b a then b else a)
+-- 6
+-- >>> (*) (if (>) a b then a else if (<) a b then b else -1) ((+) a 1)
+-- 16
