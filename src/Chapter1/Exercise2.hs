0fb1e7d2f29f777871c7399cf0e1b8900841159c
Edmund Higham <edhigham@gmail.com>
2020-01-05 00:04:56 +0000

    Exercise 1.2

---
+++ src/Chapter1/Exercise2.hs
@@ -0,0 +1,7 @@
+module Chapter1.Exercise2 where
+-- | Translate the following expressio into prefix form
+-- (5 + 4 + (2 - (3 - (6 + 4/5)))) / (3 * (6 - 2) * (2 - 7))
+--
+-- | Scheme:
+-- >>> (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7))
+
