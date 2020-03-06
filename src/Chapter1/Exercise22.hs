c2ac0fbfbe22bbff1ca4e41293b3c922b044a966
Edmund Higham <ehigham@users.noreply.github.com>
2020-03-06 16:28:58 +0000

    Finish exercise 22

--- src/Chapter1/Exercise22.hs
+++ src/Chapter1/Exercise22.hs
@@ -12,12 +12,11 @@ module Chapter1.Exercise22 (timedPrimeTest) where
 
     timedPrimeTest :: (Integral n, Show n) => n -> IO ()
     timedPrimeTest n = do
-            putStr (show n)
-            (prime, time) <- timeIt $ isPrime n
-            when prime $ reportPrime time
-            newLine
-        where
-            newLine = putStrLn ""
+        putStr (show n)
+        (prime, time) <- timeIt $ isPrime n
+        when prime $ reportPrime time
+      where
+        newLine = putStrLn ""
 
     isPrime :: (Integral n) => n -> Bool
     isPrime n = n == (smallestDivisor n)
@@ -27,35 +26,57 @@ module Chapter1.Exercise22 (timedPrimeTest) where
 
     timeIt :: a -> IO (a, NominalDiffTime)
     timeIt f = do
-            start <- getCurrentTime
-            a <- evaluate f
-            finish <- getCurrentTime
-            return (a, diffUTCTime finish start)
+        start <- getCurrentTime
+        a <- evaluate f
+        finish <- getCurrentTime
+        return (a, diffUTCTime finish start)
 
 -- | Using this procedure, write a procedure `searchForPrimes` that checks the
 -- | primality of consecutive odd integers in a specified range. Use your
--- | procedure t find the smallest prime numbers larher than 1,000; karger
--- | than 10,000; larger than 100,000; larger than 1,000,000. Note the time
--- | needed to test each prime. Since the testing algorithm has an order of
+-- | procedure to find the three smallest prime numbers larger than 1,000;
+-- | larger than 10,000; larger than 100,000; larger than 1,000,000. Note the
+-- | time needed to test each prime. Since the testing algorithm has an order of
 -- | growth of Theta(sqrt n), you should expect that the testing for primes
 -- | around 10,000 should take about sqrt 10 times as long as testing for primes
 -- | around 1,000. Do your timing data bear this out? How well do the data for
 -- | 100,000 and 1,000,000 support the (sqrt n) prediction? Is your result
 -- | compatible with the notion that programs on your machine run in time
--- | proportional to the numbre of steps required for the computation?
+-- | proportional to the number of steps required for the computation?
 
-    searchForPrimes :: (Integral n) => n -> IO[(n, NominalDiffTime)]
-    searchForPrimes n = sequence $ fmap g primes
-        where
-            primes = [p | p <- [p0..], isPrime p]
-            p0 = findFirstPrime n
-            g x = do
-                (_, time) <- timeIt $ isPrime x
-                return (x, time)
+    searchForPrimes :: (Integral n, Show n) => n -> IO ()
+    searchForPrimes n = do
+        sequence $ take 3 $ fmap timedPrimeTest $ primes
+        return ()
+      where
+        primes = [p | p <- [(from n)..], isPrime p]
+        from n
+            | isPrime n = n
+            | even n    = from (n + 1)
+            | otherwise = from (n + 2)
 
-    findFirstPrime :: (Integral n) => n -> n
-    findFirstPrime n
-        | n < 3     = 2
-        | even n    = findFirstPrime (succ n)
-        | isPrime n = n
-        | otherwise = findFirstPrime (n + 2)
+-- | As of 2020, there's too much noise in the timings below ~100,000 so we'll
+-- | start our test from here.
+
+-- >>> searchForPrimes 100000
+-- 100003 *** 0.000415891s
+-- 100019 *** 0.00036075s
+-- 100043 *** 0.00056723s
+
+-- >>> searchForPrimes 1000000
+-- 1000003 *** 0.00184107s
+-- 1000033 *** 0.001849955s
+-- 1000037 *** 0.001836786s
+
+-- >>> searchForPrimes 10000000
+-- 10000019 *** 0.005914216s
+-- 10000079 *** 0.00426524s
+-- 10000103 *** 0.00598021s
+
+-- >>> searchForPrimes 100000000
+-- 100000007 *** 0.016011287s
+-- 100000037 *** 0.019222973s
+-- 100000039 *** 0.013291527s
+
+-- | The factor of growth between orders of magnitude is ~3 ~= (sqrt 10).
+-- | We can confirm the notion that programs on your machine run in time
+-- | proportional to the number of steps required for the computation.
