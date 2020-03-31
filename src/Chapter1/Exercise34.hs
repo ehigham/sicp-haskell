5f94e4d4d6451bbab0e34c05557516324449e1b4
Edmund Higham <edhigham@gmail.com>
2020-03-31 15:36:55 +0100

    Exercise42

---
+++ src/Chapter1/Exercise34.hs
@@ -0,0 +1,21 @@
+module Chapter1.Exercise34 where
+-- | Suppose we define the procedure:
+-- | f :: (Num a) => (a -> b) -> b
+    f = ($ 2)
+
+-- | Then we have
+-- >>> f square
+-- 4
+--
+-- >>> f $ ap (*) (1 +)
+-- 6
+
+-- | What happens if we (perversely) ask the interpreter to evaluate the
+-- | combination f f? Explain.
+-- >>> f f
+-- ($ 2) f
+-- f 2
+-- ($ 2) 2
+-- 2 2
+-- | Error!
+-- | In reality, Haskell's type-checker complains
