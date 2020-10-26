c6adf18faf4ee21b3695dd1be468444329dd4e99
Edmund Higham <edhigham@gmail.com>
2020-10-26 19:18:35 +0000

    Add exercise13 - skip for now

---
+++ src/Chapter2/Exercise13.hs
@@ -0,0 +1,5 @@
+module Chapter2.Exercise13 () where
+-- | Show that under the assumption of small percentage tolerances there is a
+-- | simple formula for the approximation of percentage tolerance of the product
+-- | of two intervals in terms of the tolderaces if the factors. You may
+-- | simplify the problem by assuming that all numbers are positive.
