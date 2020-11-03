9f22d9b69b5a8312f17cf15b0b54bde22c946d2d
Edmund Higham <edhigham@gmail.com>
2020-11-03 00:25:40 +0000

    Exercise 2.23

---
+++ src/Chapter2/Exercise23.hs
@@ -0,0 +1,16 @@
+module Chapter2.Exercise23 (forEach) where
+-- | The procedure `forEach` is similar to `map`. It takes as arguments a
+-- | procedure and a list of elements. However, rather than forming a list
+-- | of the results, `forEach` just applies the procedure to each of the
+-- | elements in turn, from left to right. The values returned by applying the
+-- | procedure to the elements are not used at all - `forEach` is used with
+-- | procedures that perform an action, such as printing. For example:
+-- >>> forEach' (putStrLn . show) ([57, 321, 88] :: [Integer])
+-- 1
+-- 2
+-- 3
+-- Variable not in scope: forEach :: (a0 -> IO ()) -> [Integer] -> t
+
+    forEach :: (a -> IO b) -> [a] -> IO ()
+    forEach _ [] = return ()
+    forEach f (x:xs) = (f x) >> forEach f xs
