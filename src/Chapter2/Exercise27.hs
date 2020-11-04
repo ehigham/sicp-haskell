0664677f071d80f6885445686f10ac1047e77faa
Edmund Higham <edhigham@gmail.com>
2020-11-04 22:47:22 +0000

    Exercise 2.27 question doc

--- src/Chapter2/Exercise27.hs
+++ src/Chapter2/Exercise27.hs
@@ -1,5 +1,9 @@
 module Chapter2.Exercise27 (deepReverse) where
     import Chapter2.Exercise24 (Tree (Node))
+-- | Modify your `reverse` procedure of exercise 2.18 to produce a `deepReverse`
+-- | procedure that takes a `Tree` as argument and returns as its value the
+-- | `Tree` with its elements reversed and will all subtrees deep-reversed as
+-- | well.
 
     deepReverse :: Tree a -> Tree a
     deepReverse (Node xs) = Node $ foldl (flip ((:) . deepReverse)) [] xs
