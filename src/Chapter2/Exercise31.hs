552bafcdaacd9ba330ee7dba24f4b1ce88732e0d
Edmund Higham <edhigham@gmail.com>
2020-11-10 19:37:20 +0000

    Exercise 2.31

---
+++ src/Chapter2/Exercise31.hs
@@ -0,0 +1,10 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+module Chapter2.Exercise31 () where
+    import Chapter2.Exercise24 (Tree)
+-- | Abstract your answer to exercise 2.30 to produce a procedure `treeMap`
+-- | with the property that `squareTree` could be defined as
+-- >>> import Control.Monad (join)
+-- >>> squareTree = treeMap (join (*))
+
+    treeMap :: (a -> b) -> Tree a -> Tree b
+    treeMap = fmap
