b23fc3a7e7d6404fb8dcfa68ae38f11b59451267
Edmund Higham <edhigham@gmail.com>
2020-11-25 20:15:22 +0000

    reduce scope of differentiator

--- src/Chapter2/Symbolic/Differentiator.hs
+++ src/Chapter2/Symbolic/Differentiator.hs
@@ -3,32 +3,32 @@ module Chapter2.Symbolic.Differentiator (deriv) where
 
 import Chapter2.Symbolic.Expr (Expr(Const, Var), expr)
 
-deriv :: Expr -> Expr -> Expr
-deriv (Const _)       _         = [expr|0|]
-deriv (Var x)         (Var y)   = if x == y then [expr|1|] else [expr|0|]
-deriv [expr|$f + $g|] var       = [expr|$f' + $g'|]
+deriv :: Expr -> String -> Expr
+deriv (Const _)       _     = [expr|0|]
+deriv (Var x)         var   = if x == var then [expr|1|] else [expr|0|]
+deriv [expr|$f + $g|] var   = [expr|$f' + $g'|]
   where
     f' = deriv f var
     g' = deriv g var
-deriv [expr|$f - $g|] var       = [expr|$f' - $g'|]
+deriv [expr|$f - $g|] var   = [expr|$f' - $g'|]
   where
     f' = deriv f var
     g' = deriv g var
-deriv [expr|$f * $g|] var       = [expr|$f' * $g + $f * $g'|]
+deriv [expr|$f * $g|] var   = [expr|$f' * $g + $f * $g'|]
   where
     f' = deriv f var
     g' = deriv g var
-deriv [expr|$f / $g|] var       = [expr|($f' * $g - $f * $g')/ $g^2|]
+deriv [expr|$f / $g|] var   = [expr|($f' * $g - $f * $g')/ $g^2|]
  where
     f' = deriv f var
     g' = deriv g var
-deriv [expr|-$f|]     var       = [expr|-$f'|]
+deriv [expr|-$f|]     var   = [expr|-$f'|]
   where
     f' = deriv f var
-deriv [expr|+$f|]     var       = [expr|+$f'|]
+deriv [expr|+$f|]     var   = [expr|+$f'|]
   where
     f' = deriv f var
-deriv [expr|ln $f|]   var       = [expr|1 / $f * $f'|]
+deriv [expr|ln $f|]   var   = [expr|1 / $f * $f'|]
   where
     f' = deriv f var
 -- | Exercise 2.56
@@ -37,8 +37,8 @@ deriv [expr|ln $f|]   var       = [expr|1 / $f * $f'|]
 -- @
 --   d(f^g)/dx = f^(g-1) * (g * df/dx + f dg/dx ln f)
 -- @
-deriv [expr|$f ^ $g|] var       = [expr|$f^($g - 1)*($g*$f' + $f*(ln $f)*$g')|]
+deriv [expr|$f ^ $g|] var   = [expr|$f^($g - 1)*($g*$f' + $f*(ln $f)*$g')|]
   where
     f' = deriv f var
     g' = deriv g var
-deriv _               _         = error "unknown expression type"
+deriv _               _     = error "unknown expression type"
--- test/Chapter2/TExercise56.hs
+++ test/Chapter2/TExercise56.hs
@@ -6,34 +6,34 @@ import Test.HUnit
 
 tests :: Test
 tests = TestList [
-    TestLabel "Constant" $ [expr|0|] ~=? deriv [expr|1|] [expr|x|],
-    TestLabel "Same var" $ [expr|1|] ~=? deriv [expr|x|] [expr|x|],
-    TestLabel "Different var" $ [expr|0|] ~=? deriv [expr|z|] [expr|x|],
+    TestLabel "Constant" $ [expr|0|] ~=? deriv [expr|1|] "x",
+    TestLabel "Same var" $ [expr|1|] ~=? deriv [expr|x|] "x",
+    TestLabel "Different var" $ [expr|0|] ~=? deriv [expr|z|] "x",
     TestLabel "Addition" $ TestList [
-        [expr|1 + 1|] ~=? deriv [expr|x + x|] [expr|x|],
-        [expr|1 + 0|] ~=? deriv [expr|x + 1|] [expr|x|],
-        [expr|0 + 0|] ~=? deriv [expr|y + y|] [expr|x|]
+        [expr|1 + 1|] ~=? deriv [expr|x + x|] "x",
+        [expr|1 + 0|] ~=? deriv [expr|x + 1|] "x",
+        [expr|0 + 0|] ~=? deriv [expr|y + y|] "x"
         ],
     TestLabel "Subtraction" $ TestList [
-        [expr|1 - 1|] ~=? deriv [expr|x - x|] [expr|x|],
-        [expr|1 - 0|] ~=? deriv [expr|x - 1|] [expr|x|],
-        [expr|0 - 0|] ~=? deriv [expr|y - y|] [expr|x|]
+        [expr|1 - 1|] ~=? deriv [expr|x - x|] "x",
+        [expr|1 - 0|] ~=? deriv [expr|x - 1|] "x",
+        [expr|0 - 0|] ~=? deriv [expr|y - y|] "x"
         ],
     TestLabel "Multiplication" $ TestList [
-        [expr|1 * x + x * 1|] ~=? deriv [expr|x * x|] [expr|x|],
-        [expr|1 * y + x * 0|] ~=? deriv [expr|x * y|] [expr|x|],
-        [expr|0 * x + 2 * 1|] ~=? deriv [expr|2 * x|] [expr|x|]
+        [expr|1 * x + x * 1|] ~=? deriv [expr|x * x|] "x",
+        [expr|1 * y + x * 0|] ~=? deriv [expr|x * y|] "x",
+        [expr|0 * x + 2 * 1|] ~=? deriv [expr|2 * x|] "x"
         ],
     TestLabel "Division" $ TestList [
-        [expr|(0 * x - 1 * 1)/(x^2)|] ~=? deriv [expr|1 / x|] [expr|x|],
-        [expr|(0 * y - 1 * 0)/(y^2)|] ~=? deriv [expr|1 / y|] [expr|x|]
+        [expr|(0 * x - 1 * 1)/(x^2)|] ~=? deriv [expr|1 / x|] "x",
+        [expr|(0 * y - 1 * 0)/(y^2)|] ~=? deriv [expr|1 / y|] "x"
         ],
     TestLabel "Exponentiation" $ TestList [
-        [expr|x^(2-1)*(2*1 + x*(ln x)*0)|] ~=? deriv [expr|x^2|] [expr|x|],
-        [expr|x^(x-1)*(x*1 + x*(ln x)*1)|] ~=? deriv [expr|x^x|] [expr|x|]
+        [expr|x^(2-1)*(2*1 + x*(ln x)*0)|] ~=? deriv [expr|x^2|] "x",
+        [expr|x^(x-1)*(x*1 + x*(ln x)*1)|] ~=? deriv [expr|x^x|] "x"
         ],
     TestLabel "Natural Log" $ TestList [
-        [expr|1/x * 1|] ~=? deriv [expr|ln x|] [expr|x|],
-        [expr|1/x * 0|] ~=? deriv [expr|ln x|] [expr|y|]
+        [expr|1/x * 1|] ~=? deriv [expr|ln x|] "x",
+        [expr|1/x * 0|] ~=? deriv [expr|ln x|] "y"
         ]
     ]
