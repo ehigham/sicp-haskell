71197c056d058c6d66da4aa3480c912a5d83987f
Edmund Higham <edhigham@gmail.com>
2020-11-28 14:59:03 +0000

    rename HasKey -> Known

--- src/Chapter2/Exercise66.hs
+++ src/Chapter2/Exercise66.hs
@@ -1,16 +1,16 @@
 {-# OPTIONS_GHC -Wno-unsafe #-}
 
-module Chapter2.Exercise66 (lookup, HasKey) where
+module Chapter2.Exercise66 (lookup, Known) where
 
 import Prelude hiding (lookup)
 import Chapter2.Exercise63 (Tree (Empty, Node))
 -- Implement the `lookup` procedure for the case where the set of records is
 -- structured as a binary tree, ordered by the numerical values of the keys.
 
-class HasKey s where
+class Known s where
     key :: Ord a => s -> a
 
-lookup :: (Ord a, HasKey b) => a -> Tree b -> Maybe b
+lookup :: (Ord a, Known b) => a -> Tree b -> Maybe b
 lookup _ Empty        = Nothing
 lookup a (Node l x r) | a == key x = Just x
                       | a < key x  = lookup a l
